ce157f7a2f73545e15c4b2e979149faf55e12aed
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-23 02:38:57 +0600

    display complete

---
+++ js/Api.js
@@ -0,0 +1,42 @@
+function postLoad(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json.title))
+}
+
+
+
+function userLoad(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>getUser(data));
+}
+
+
+function getUser(data){
+    const div = document.getElementById('comment-box');
+    const userHead = document.getElementById('user-head');
+    userHead.style.display='block';
+    for(const user of data){
+         const li = document.createElement('li');
+         li.innerText = user.name;
+         div.appendChild(li);
+    } 
+}
+function loadComment(){
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res=>res.json())
+    .then(data=>getComment(data))
+}
+
+function getComment(props){
+    const div = document.getElementById('comment-box');
+    const userComent = document.getElementById('user-coment');
+    userComent.style.display='block';
+    for(const coment of props){
+        const li = document.createElement('li');
+        li.innerText = coment.email;
+        div.appendChild(li);
+    }
+}
+
